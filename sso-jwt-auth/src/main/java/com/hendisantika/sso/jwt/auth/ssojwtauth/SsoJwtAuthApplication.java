305da9f4f5f1738387bf53773d73ab4d8d0b22c5
Hendi Santika <hendisantika@gmail.com>
2017-10-07 22:51:19 +0700

    #13 - modified gitignore - sso-jwt-auth

---
+++ sso-jwt-auth/src/main/java/com/hendisantika/sso/jwt/auth/ssojwtauth/SsoJwtAuthApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.sso.jwt.auth.ssojwtauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoJwtAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoJwtAuthApplication.class, args);
+	}
+}
---
+++ sso-jwt-auth/src/test/java/com/hendisantika/sso/jwt/auth/ssojwtauth/SsoJwtAuthApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.sso.jwt.auth.ssojwtauth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SsoJwtAuthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
