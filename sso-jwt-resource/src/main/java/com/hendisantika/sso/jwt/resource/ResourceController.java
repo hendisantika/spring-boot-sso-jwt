8f81bd58481ca8281527250cff4c91bf52b53c59
Hendi Santika <hendisantika@gmail.com>
2017-10-07 22:39:58 +0700

    #7 - Added Resource Controller  - sso-jwt-resource

--- sso-jwt-resource/src/main/java/com/hendisantika/sso/jwt/resource/ResourceController.java
+++ sso-jwt-resource/src/main/java/com/hendisantika/sso/jwt/resource/ResourceController.java
@@ -1,5 +1,10 @@
 package com.hendisantika.sso.jwt.resource;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : sso-jwt
@@ -10,5 +15,24 @@ package com.hendisantika.sso.jwt.resource;
  * Time: 22.37
  * To change this template use File | Settings | File Templates.
  */
+
+@Controller
 public class ResourceController {
+    private static final String jwtTokenCookieName = "JWT-TOKEN";
+
+    @RequestMapping("/")
+    public String home() {
+        return "redirect:/protected-resource";
+    }
+
+    @RequestMapping("/protected-resource")
+    public String protectedResource() {
+        return "protected-resource";
+    }
+
+    @RequestMapping("/logout")
+    public String logout(HttpServletResponse httpServletResponse) {
+        CookieUtil.clear(httpServletResponse, jwtTokenCookieName);
+        return "redirect:/";
+    }
 }
