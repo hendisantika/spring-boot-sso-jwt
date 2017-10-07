7d6257ba1aec9e80749156130fc6c958bf4fc747
Hendi Santika <hendisantika@gmail.com>
2017-10-07 22:40:47 +0700

    #8 - Modified Main APp

--- sso-jwt-resource/src/main/java/com/hendisantika/sso/jwt/resource/SpringBootSsoJwtApplication.java
+++ sso-jwt-resource/src/main/java/com/hendisantika/sso/jwt/resource/SpringBootSsoJwtApplication.java
@@ -1,10 +1,34 @@
 package com.hendisantika.sso.jwt.resource;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Collections;
 
 @SpringBootApplication
-public class SpringBootSsoJwtApplication {
+public class SpringBootSsoJwtApplication extends SpringBootServletInitializer {
+	@Value("${services.auth}")
+	private String authService;
+
+	@Bean
+	public FilterRegistrationBean jwtFilter() {
+		final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+		registrationBean.setFilter(new JwtFilter());
+		registrationBean.setInitParameters(Collections.singletonMap("services.auth", authService));
+		registrationBean.addUrlPatterns("/protected-resource");
+
+		return registrationBean;
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootSsoJwtApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSsoJwtApplication.class, args);
