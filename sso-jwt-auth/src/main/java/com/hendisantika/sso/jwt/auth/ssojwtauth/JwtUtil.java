f48ec40a04c0c73b392c8143fc0c6659d055765a
Hendi Santika <hendisantika@gmail.com>
2017-10-07 22:56:31 +0700

    #17 - Added JWT util - sso-jwt-auth

---
+++ sso-jwt-auth/src/main/java/com/hendisantika/sso/jwt/auth/ssojwtauth/JwtUtil.java
@@ -0,0 +1,38 @@
+package com.hendisantika.sso.jwt.auth.ssojwtauth;
+
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : sso-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 07/10/17
+ * Time: 22.55
+ * To change this template use File | Settings | File Templates.
+ */
+public class JwtUtil {
+    public static String generateToken(String signingKey, String subject) {
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+
+        JwtBuilder builder = Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(now)
+                .signWith(SignatureAlgorithm.HS256, signingKey);
+
+        return builder.compact();
+    }
+
+    public static String getSubject(HttpServletRequest httpServletRequest, String jwtTokenCookieName, String signingKey){
+        String token = CookieUtil.getValue(httpServletRequest, jwtTokenCookieName);
+        if(token == null) return null;
+        return Jwts.parser().setSigningKey(signingKey).parseClaimsJws(token).getBody().getSubject();
+    }
+}
