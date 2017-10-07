2a210b550a075014625747a1004472fb1f0605ec
Hendi Santika <hendisantika@gmail.com>
2017-10-07 22:58:34 +0700

    #19 - Modifed Main APp

--- sso-jwt-auth/src/main/java/com/hendisantika/sso/jwt/auth/ssojwtauth/SsoJwtAuthApplication.java
+++ sso-jwt-auth/src/main/java/com/hendisantika/sso/jwt/auth/ssojwtauth/SsoJwtAuthApplication.java
@@ -2,9 +2,15 @@ package com.hendisantika.sso.jwt.auth.ssojwtauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SsoJwtAuthApplication {
+public class SsoJwtAuthApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SsoJwtAuthApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SsoJwtAuthApplication.class, args);
