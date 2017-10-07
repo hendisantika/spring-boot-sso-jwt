8abb8510e25f5a7ad844f809c4820e621746b470
Hendi Santika <hendisantika@gmail.com>
2017-10-07 22:33:23 +0700

    #3 - Modified structure sso-jwt project

---
+++ sso-jwt-resource/src/main/java/com/hendisantika/sso/jwt/resource/SpringBootSsoJwtApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.sso.jwt.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSsoJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSsoJwtApplication.class, args);
+	}
+}
