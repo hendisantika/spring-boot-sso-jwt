3b7d8e90be5b8dc0d2897ae3a08ad0edb5ca85cd
Hendi Santika <hendisantika@gmail.com>
2017-10-07 22:38:52 +0700

    #4 - Added Cookie Util - sso-jwt-resource

---
+++ sso-jwt-resource/src/main/java/com/hendisantika/sso/jwt/resource/JwtUtil.java
@@ -0,0 +1,14 @@
+package com.hendisantika.sso.jwt.resource;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : sso-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 07/10/17
+ * Time: 22.36
+ * To change this template use File | Settings | File Templates.
+ */
+public class JwtUtil {
+}
