92a3ea56726cfe1276cee8a6710583f2664e93e9
Hendi Santika <hendisantika@gmail.com>
2017-10-07 22:39:16 +0700

    #5 - Added JWT Filter - sso-jwt-resource

--- sso-jwt-resource/src/main/java/com/hendisantika/sso/jwt/resource/JwtFilter.java
+++ sso-jwt-resource/src/main/java/com/hendisantika/sso/jwt/resource/JwtFilter.java
@@ -1,5 +1,14 @@
 package com.hendisantika.sso.jwt.resource;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : sso-jwt
@@ -10,5 +19,21 @@ package com.hendisantika.sso.jwt.resource;
  * Time: 22.36
  * To change this template use File | Settings | File Templates.
  */
-public class JwtFilter {
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+    private static final String jwtTokenCookieName = "JWT-TOKEN";
+    private static final String signingKey = "signingKey";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        String username = JwtUtil.getSubject(httpServletRequest, jwtTokenCookieName, signingKey);
+        if(username == null){
+            String authService = this.getFilterConfig().getInitParameter("services.auth");
+            httpServletResponse.sendRedirect(authService + "?redirect=" + httpServletRequest.getRequestURL());
+        } else{
+            httpServletRequest.setAttribute("username", username);
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
+        }
+    }
 }
