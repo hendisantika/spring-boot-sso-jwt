52dcb87c28390a6af1bf90d215aabe9d941b9af6
Hendi Santika <hendisantika@gmail.com>
2017-10-07 22:55:40 +0700

    #16 - Added Cookie Util

---
+++ sso-jwt-auth/src/main/java/com/hendisantika/sso/jwt/auth/ssojwtauth/CookieUtil.java
@@ -0,0 +1,42 @@
+package com.hendisantika.sso.jwt.auth.ssojwtauth;
+
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : sso-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 07/10/17
+ * Time: 22.55
+ * To change this template use File | Settings | File Templates.
+ */
+public class CookieUtil {
+    public static void create(HttpServletResponse httpServletResponse, String name, String value, Boolean secure, Integer maxAge, String domain) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setSecure(secure);
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(maxAge);
+        cookie.setDomain(domain);
+        cookie.setPath("/");
+        httpServletResponse.addCookie(cookie);
+    }
+
+    public static void clear(HttpServletResponse httpServletResponse, String name) {
+        Cookie cookie = new Cookie(name, null);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(0);
+        httpServletResponse.addCookie(cookie);
+    }
+
+    public static String getValue(HttpServletRequest httpServletRequest, String name) {
+        Cookie cookie = WebUtils.getCookie(httpServletRequest, name);
+        return cookie != null ? cookie.getValue() : null;
+    }
+}
